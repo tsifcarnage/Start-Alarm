2bdc5d4237814a6e2d58b03a568e14f2aa254286
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-08-14 10:14:34 +0200

    Update Layout.vue

--- src/components/nav/Layout.vue
+++ src/components/nav/Layout.vue
@@ -5,17 +5,19 @@ import Header from "./Header.vue";
 </script>
 
 <template>
-  <div class="flex min-h-screen bg-(--bg-secondary) font-sans antialiased">
-    <!-- Sidebar / Menu à gauche -->
+  <div class="flex h-screen overflow-hidden bg-(--bg-secondary) font-sans antialiased">
+    <!-- Sidebar / Menu à gauche (fixe) -->
     <Sidebar />
 
     <!-- Contenu principal à droite -->
     <div class="flex flex-1 flex-col overflow-hidden">
+      <!-- Header (fixe) -->
       <Header />
 
+      <!-- Zone de contenu défilable -->
       <main class="flex-1 overflow-y-auto p-6">
         <div
-          class=" min-[1408px]:flex-row  min-h-full mx-auto flex flex-col min-[1408px]:items-start items-center justify-between gap-12 bg-(--bg-primary) p-8 rounded-xl shadow-sm "
+          class="mx-auto flex min-h-full flex-col items-center justify-between gap-12 rounded-xl bg-(--bg-primary) p-8 shadow-sm min-[1408px]:flex-row min-[1408px]:items-start"
         >
           <RouterView />
         </div>
